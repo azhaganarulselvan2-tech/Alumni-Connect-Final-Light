1ab2507b3e5401fe81c71bb4f3a6728d92f5fba8
azhaganarulselvan2-tech <azhaganarulselvan2@gmail.com>
2025-09-16 20:10:24 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: { extend: {} },
+  plugins: [],
+  
+  theme: {
+    extend: {
+      colors: {
+        background: '#f9f9f9',
+        surface: '#ffffff',
+        text: '#1a1a1a',
+        primary: '#1976d2',
+      },
+    },
+  },
+
+  
+}
